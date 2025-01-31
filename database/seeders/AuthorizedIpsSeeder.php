4fee7d7b3bdfecd9e0572f6d4cf5b31076e80349
johnftv85 <j.fredytv@gmail.com>
2025-01-31 09:23:36 -0500

    autorize ip

--- database/seeders/AuthorizedIpsSeeder.php
+++ database/seeders/AuthorizedIpsSeeder.php
@@ -13,8 +13,8 @@ class AuthorizedIpsSeeder extends Seeder
      */
     public function run(): void
     {
-        AuthorizedIp::create(['ip_address' => '192.168.1.1', 'description' => 'Servidor interno']);
-        AuthorizedIp::create(['ip_address' => '35.226.95.247', 'description' => 'Servidor 20']);
+        AuthorizedIp::create(['ip_address' => '104.154.206.97', 'description' => 'Servidor de pruebas']);
+        AuthorizedIp::create(['ip_address' => '34.94.155.169', 'description' => 'Servidor 17']);
         AuthorizedIp::create(['ip_address' => '127.0.0.1', 'description' => 'Servidor Local']);
     }
 }
